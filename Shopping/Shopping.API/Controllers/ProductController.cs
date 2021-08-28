66244826c980a09f4a792c0b2361a948d3fd47bc
Lukas Nguyen <lukas.nguyen@contemi.com>
2021-08-28 15:42:57 +0700

    Added Shopping API microservice and consume data from this API.

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shopping.API.Data;
+using Shopping.API.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Product>))]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            return Ok(ProductContext.Products);
+        }
+    }
+}
